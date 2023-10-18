cef0710855d0363c2b654269e24cf044e8987fe9
Srinivas Sarkar <srinivassarkar07@gmail.com>
2023-10-19 04:26:23 +0530

    create UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
 import { useEffect, useState } from "react";
-import { BsSearch } from "react-icons/bs";
-import DateTime from "./Components/DateTime";
+import {
+  BsSearch,
+  BsFillCloudsFill,
+  BsFillCloudHazeFill,
+  BsFillCloudRainFill,
+  BsFillSunFill,
+  BsFillCloudSnowFill,
+  BsFillCloudLightningRainFill,
+  BsWind,
+} from "react-icons/bs";
+import { LuLoader, LuWaves } from "react-icons/lu";
+import { AiOutlineEye } from "react-icons/ai";
+import { CiTempHigh } from "react-icons/ci";
+
 
 export default function App() {
   const [searchCity, setSearchCity] = useState("");
   const [weather, setWeather] = useState({});
+  const [animate, setAnimate] = useState(false);
+  const date = new Date();
 
   const apiKey = "0233c7695f99f4daa61475a720a71307";
 
   async function fetchWeatherData() {
     if (searchCity) {
-      const baseUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&units=metric&&APPID=${apiKey}`;
+      const baseUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&units=metric&APPID=${apiKey}`;
       const data = await fetch(baseUrl);
       const result = await data.json();
       setWeather(result);
-      console.log(result);
     }
   }
 
@@ -26,17 +39,66 @@ export default function App() {
 
   useEffect(() => {
     fetchWeatherData();
-  }, [searchCity]); // This will trigger the effect whenever searchCity changes
+  }, [searchCity]);
+
+  function handleSubmit(e) {
+    if (searchCity != "") {
+      fetchWeatherData();
+    }
+
+    e.preventDefault();
+  }
+
+  if (!weather) {
+    <LuLoader className="text-5xl animate-spin" />;
+  }
+
+
+  //if input is empty 
+
+
+  
+  // Define the icon variable with a default value
+  let icon = null;
+
+  if (weather && weather.weather && weather.weather.length > 0) {
+    // Check if weather and weather.weather are defined
+    switch (weather.weather[0].main) {
+      case "Clouds":
+        icon = <BsFillCloudsFill />;
+        break;
+      case "Haze":
+        icon = <BsFillCloudHazeFill />;
+        break;
+      case "Rain":
+        icon = <BsFillCloudRainFill />;
+        break;
+      case "Clear":
+        icon = <BsFillSunFill />;
+        break;
+      case "Drizzle":
+        icon = <BsFillCloudRainFill />;
+        break;
+      case "Snow":
+        icon = <BsFillCloudSnowFill />;
+        break;
+      case "Thunderstorm":
+        icon = <BsFillCloudLightningRainFill />;
+        break;
+    }
+  }
 
   return (
     <>
-      <div>
+      <div className="w-full min-h-screen bg-gray-950 flex items-center  justify-center">
         {/* Button */}
-        <div className="flex justify-center items-center bg-gray-500">
-          <div className="container mx-auto px-4 flex items-center">
+        <div className="w-full py-3 max-w-md bg-gray-800 mx-auto  rounded-lg p-4">
+          <div 
+          
+          className="container mx-auto px-11 flex items-center mt-3">
             <input
               type="text"
-              className="bg-white rounded-l-full px-5 py-2 w-64 focus:outline-none focus:shadow-md"
+              className="bg-white rounded-l-full px-5 py-2  w-full sm:w-64 focus:outline-none focus:shadow-md"
               placeholder="Enter City..."
               value={searchCity}
               onChange={(e) => {
@@ -46,29 +108,75 @@ export default function App() {
             />
             <button
               type="submit"
-              className="bg-yellow-400 hover-bg-yellow-500 text-gray-900 rounded-r-full py-3 px-5 focus-shadow-md"
-              onClick={fetchWeatherData}
+              className="bg-yellow-400 hover:bg-yellow-500 text-gray-900 rounded-r-full py-3 px-5 focus-shadow-md"
+              onClick={() => handleSubmit()}
             >
               <BsSearch className="h-4 w-5" />
             </button>
           </div>
-        </div>
 
-        {/* {Body} */}
-        <div>
-          <p>name: {weather.name}</p>
-          <p>
-            <DateTime />
-          </p>
-          <p>temperature: {weather.main && weather.main.temp}°C</p>
-          <p>type: {weather.weather && weather.weather[0].main}</p>
-          <p>
-            description: {weather.weather && weather.weather[0].description}
-          </p>
-          <p>feels like : {weather.weather && weather.main.feels_like} °C</p>
-          <p>visibility: {weather.weather && weather.visibility / 1000} km</p>
-          <p>humidity: {weather.weather && weather.main.humidity}</p>
-          <p>wind speed: {weather.weather && weather.wind.speed} m\s</p>
+          <div className="text-white">
+            {/* head */}
+            <div className="mt-6 flex flex-col items-center justify-center sm:flex-row">
+              <div className="text-6xl mb-4 sm:mb-0">{icon}</div>
+              <div className="text-2xl mx-4 font-semibold text-center sm:text-left">
+                {weather.weather ? (
+                  <div className="text-2xl">
+                    {weather.name}
+                    {weather.sys && `, ${weather.sys.country}`}
+                  </div>
+                ) : null}
+                {weather.weather ? (
+                  <div className="text-sm">
+                    {date.getUTCDate()}/{date.getUTCMonth() + 1}/
+                    {date.getUTCFullYear()}
+                  </div>
+                ) : null}
+              </div>
+            </div>
+
+            <div className="flex flex-col mt-8 items-center text-center sm:text-left sm:mx-14">
+              {weather.main && (
+                <div className="text-5xl sm:text-[85px] leading-none">
+                  {parseInt(weather.main.temp)}°C
+                </div>
+              )}
+              <div className="text-lg sm:text-xl">
+                {weather.weather && weather.weather[0].main}
+              </div>
+            </div>
+            {/* body */}
+            <div className="flex flex-col mt-12 sm:flex-row justify-between ">
+              <div className="flex flex-col">
+                {weather.main && (
+                  <div className="flex  items-center mb-2">
+                    <CiTempHigh className="mr-2 " />
+                    Feels Like {parseInt(weather.main.feels_like)} °C
+                  </div>
+                )}
+                {weather.main && (
+                  <div className="flex items-center">
+                    <AiOutlineEye className="mr-2 " />
+                    Visibility {weather.visibility / 1000} km
+                  </div>
+                )}
+              </div>
+              <div className="flex flex-col">
+                {weather.main && (
+                  <div className="flex items-center mb-2">
+                    <LuWaves className="mr-2" />
+                    Humidity {weather.main.humidity} %
+                  </div>
+                )}
+                {weather.wind && (
+                  <div className="flex items-center">
+                    <BsWind className="mr-2" />
+                    Wind {weather.wind.speed} m/s
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
