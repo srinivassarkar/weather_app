56a4c8a5ff2b79d8c9fa75b550c3dd74829be8fa
Srinivas Sarkar <srinivassarkar07@gmail.com>
2023-10-18 20:26:03 +0530

    add weather api fetch details in console

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BsSearch } from "react-icons/bs";
 export default function App() {
-  const [searchCity, setSearchCity] = useState("Delhi");
-  function handleChange(e) {
-    setSearchCity(e.target.value);
+  const [searchCity, setSearchCity] = useState("");
+
+  const apiKey = "0233c7695f99f4daa61475a720a71307";
+  const baseUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&APPID=${apiKey}`;
+  async function searchPressed() {
+    const data = await fetch(baseUrl);
+    const weather = await data.json();
+    console.log(weather);
   }
+  function handleEnterKey(e) {
+    if (e.key == "Enter") {
+      searchPressed();
+    }
+  }
+
+  useEffect(() => {
+    searchPressed();
+  }, []);
 
   return (
     <>
@@ -17,13 +31,16 @@ export default function App() {
               className="bg-white rounded-l-full px-5 py-2 w-64 focus:outline-none focus:shadow-md"
               placeholder="Enter City..."
               value={searchCity}
-              onChange={handleChange}
+              onChange={(e) => {
+                setSearchCity(e.target.value);
+              }}
+              onKeyDown={handleEnterKey}
             />
             <button
               type="submit"
               className="bg-yellow-400 hover:bg-yellow-500 text-gray-900 rounded-r-full py-3 px-5  focus:shadow-md"
             >
-              <BsSearch className="h-4 w-5 " />
+              <BsSearch className="h-4 w-5 " onClick={searchPressed} />
             </button>
             {/* {searchCity} */}
           </div>
@@ -32,7 +49,7 @@ export default function App() {
         {/* {Body} */}
 
         <div>
-          <div>City name and weather</div>
+          <div>City name </div>
           <div>Other details</div>
         </div>
       </div>
