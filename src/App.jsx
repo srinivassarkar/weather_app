76614512f19968ab1ba0155aff03f510f76d6e5b
Srinivas Sarkar <srinivassarkar07@gmail.com>
2023-10-18 18:17:47 +0530

    add search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,41 @@
+import { useState } from "react";
+import { BsSearch } from "react-icons/bs";
 export default function App() {
-  return <div>App</div>;
+  const [searchCity, setSearchCity] = useState("Delhi");
+  function handleChange(e) {
+    setSearchCity(e.target.value);
+  }
+
+  return (
+    <>
+      <div>
+        {/* Button */}
+        <div className="flex justify-center items-center bg-gray-500">
+          <div className="container mx-auto px-4 flex items-center">
+            <input
+              type="text"
+              className="bg-white rounded-l-full px-5 py-2 w-64 focus:outline-none focus:shadow-md"
+              placeholder="Enter City..."
+              value={searchCity}
+              onChange={handleChange}
+            />
+            <button
+              type="submit"
+              className="bg-yellow-400 hover:bg-yellow-500 text-gray-900 rounded-r-full py-3 px-5  focus:shadow-md"
+            >
+              <BsSearch className="h-4 w-5 " />
+            </button>
+            {/* {searchCity} */}
+          </div>
+        </div>
+
+        {/* {Body} */}
+
+        <div>
+          <div>City name and weather</div>
+          <div>Other details</div>
+        </div>
+      </div>
+    </>
+  );
 }
